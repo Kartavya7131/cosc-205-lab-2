5740259bd43251f1beb8c4e6909a87b2bdabe800
KIverson11 <kiverson11@gmail.com>
2025-01-23 13:28:56 -0800

    add all

--- CompanySignupHandler.php
+++ CompanySignupHandler.php
@@ -3,9 +3,15 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    
-
     $sql =  mysqli_connect("localhost", "cs213user", "letmein", "CourseFinalDB");
+
+    $companyName = fixInput($_POST['cname']);
+    $companyDescription = fixInput($_POST['cdesc']);
+    $industry = fixInput($_POST['industry']);
+    $CEOName = fixInput($_POST['ceoname']);
+    $YearRev = fixInput($_POST['yearrevenu']);
+    $empCount = fixInput($_POST['employeeCount']);
+    $yearFounded = fixInputq($_POST['yearfounded']);
 
     header("Location: LoginPage.php");
     exit();
