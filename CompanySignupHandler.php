23cb6ba86e02dd3263a2795f32c11f4ad8b45ba3
ThatWaluigi <ThatWaluigi@gmail.com>
2025-01-23 10:24:49 -0800

    StudentSignup Integrated

--- CompanySignupHandler.php
+++ CompanySignupHandler.php
@@ -1,1 +1,19 @@
+<?php
 session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    
+
+    $sql =  mysqli_connect("localhost", "cs213user", "letmein", "CourseFinalDB");
+
+    header("Location: LoginPage.php");
+    exit();
+}
+
+function fixInput($in) {
+    $in = trim($in);
+    $in = stripslashes($in);
+    return $in;
+}
+?>
