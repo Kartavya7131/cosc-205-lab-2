747a8282fa9fd9fed78b65b4f50c7f551d7ac48d
KIverson11 <kiverson11@gmail.com>
2025-01-23 10:49:10 -0800

    creating a spot to upload resume

--- StudentSignupHandler.php
+++ StudentSignupHandler.php
@@ -9,13 +9,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = fixInput($_POST['username']);
     $pass = fixInput($_POST['password']);
     $education = fixInput($_POST['education']);
+    $fileUpload = $_FILES['ResumeName']['tmp_name'];
 
     $sql =  mysqli_connect("localhost", "cs213user", "letmein", "CourseFinalDB");
 
     $query = mysqli_query($sql, "INSERT INTO student values ('" . strtolower($user) 
     . "', SHA1('" . $pass . "'), '"
     . $fname . ", " . $lname . ", " . $age 
-    . ", " . $email . ", " . $education . ")") or die(mysqli_error($sql));
+    . ", " . $email . ", " . $education . ", " . file_get_contents($fileUpload) . ")") or die(mysqli_error($sql));
 
     $_SESSION['username'] = $user;
 
@@ -28,4 +29,16 @@ function fixInput($in) {
     $in = stripslashes($in);
     return $in;
 }
+
+$target_path = tmp_name;
+        
+$target_path = $target_path . basename($_FILES['fileupload']['name']);
+
+    // if (move_uploaded_file($_FILES['fileupload']['tmp_name'], $target_path)) {
+    //         echo "The file \"". basename($_FILES['fileupload']['name']). 
+    //             "\" has been uploaded. <br />";
+    //            echo '<a href="userlogin.php">Back to your page</a>';
+    // } else {
+    //         echo "There was an error uploading the file, please try again! <br />";
+    // }
 ?>
