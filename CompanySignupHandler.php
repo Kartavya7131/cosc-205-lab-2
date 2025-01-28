fbd93ff5cda1cea8d8bf74ee721913c0c66535b7
ThatWaluigi <ThatWaluigi@gmail.com>
2025-01-27 16:50:58 -0800

    fixed small issues

--- CompanySignupHandler.php
+++ CompanySignupHandler.php
@@ -14,11 +14,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $CEOName = fixInput($_POST['ceoname']);
     $YearRev = fixInput($_POST['yearrevenu']);
     $empCount = fixInput($_POST['employeeCount']);
-    $yearFounded = fixInputq($_POST['yearfounded']);
+    $yearFounded = fixInput($_POST['yearfounded']);
 
     $sprintf = sprintf("INSERT INTO %s values ('%s', '%s', '%s', SHA1('%s'), '%s', '%s', %d, %d, %d)", "employer", $companyName, $companyDescription, $email, $pass, $industry, $CEOName, $YearRev, $empCount, $yearFounded);
-
-    echo $sprintf;
 
     $query = mysqli_query($sql, $sprintf) or die(mysqli_error($sql));
 
--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 function connectDB() {
-    $conn = mysqli_connect("localhost", "cs213user", "letmein", "job_board");
+    $conn = mysqli_connect("localhost", "cs205user", "letmein", "job_board");
 
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
