8852e95d19fc60abea875d91ecec514b4a66c00e
ThatWaluigi <ThatWaluigi@gmail.com>
2025-02-01 13:14:18 -0800

    GET for JobId for Application

--- JobBoardBtn.js
+++ JobBoardBtn.js
@@ -2,5 +2,5 @@ $("#Helper").on("click", ".JobApplyButton", (event) => {
     var self = $(event.currentTarget).parent().parent();
 
     var id = parseInt((self[0].id).split("Job-")[1]);
-    console.log(id);
+    window.location.href = "ApplicationForm.html?jobId=" + id;
 });
