f9fffec543ccde18f5b584b97f86e875db00bcdd
JordanNewman07 <jordannewman211@gmail.com>
2025-02-02 14:54:28 -0800

    Added php functionality to add a resume to the database

--- ApplicationHandler.php
+++ ApplicationHandler.php
@@ -1,3 +1,14 @@
 <?php
+    header('Content-Type: application/json');
     session_start();
+
+    include "dbFunctions.php";
+
+    $username = $_SESSION['Username'];
+    $jobID = $_POST['id'];
+    $email = $_SESSION['email'];
+    $resume = $_POST['resume'];
+
+    $sql = "INSERT INTO `stu_application`(`Username`, `Job_ID`, `Email`, `Resume`) VALUES ('$username',$jobID,'$email', LOAD_FILE('$resume'))";
+    $result = QueryDB($sql);
 ?>
