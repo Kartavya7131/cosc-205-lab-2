2237936ed6abe7f15882aaba0236c7f54fed74d5
KIverson11 <kiverson11@gmail.com>
2025-02-02 15:58:22 -0800

    bare bones resume uploader

--- ApplicationHandler.php
+++ ApplicationHandler.php
@@ -1,3 +1,28 @@
 <?php
-    session_start();
+   session_start();
+   include "dbFunctions.php";
+   include "JobRenderer.php";
+   $conn = connectDB();
+
+    if (!isset($_SESSION["LoggedIn"])){
+        $_SESSION["LoggedIn"] = false;
+        $_SESSION['isStudent'] = true;
+    }
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload PDF</title>
+</head>
+<body>
+    <h2>Upload Your Resume</h2>
+    <form action="jobposting.php" method="POST">
+        <input type="file" name="pdf_file" accept="application/pdf" required>
+        <button type="submit">Upload</button>
+    </form>
+</body>
+</html>
