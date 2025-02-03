06bc7c8967b4f51b99ad5fd0fd6569178e20622b
ThatWaluigi <ThatWaluigi@gmail.com>
2025-02-03 15:44:03 -0800

    Fixed Job ID Err

--- AddResume.php
+++ AddResume.php
@@ -10,7 +10,7 @@
     <h2>Upload Your Resume</h2>
     <form action="Handlers/ApplicationHandler.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="pdf_file" accept="application/pdf" required>
-        <input type="hidden" name="jobId" value=<?php echo '$_GET["jobId"]';?>>
+        <input type="hidden" name="jobId" value=<?php echo $_GET['jobId'];?>>
         <button type="submit" name="submit">Upload</button>
     </form>
 </body>
